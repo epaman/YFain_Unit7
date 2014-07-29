fafbad53e5fa72ca93331d58abe7c730d032d34f
Dmitriy Lois <epaman@rambler.ru>
2014-07-29 20:50:26 +0300

    update BikeOrder

--- src/BikeOrder.java
+++ src/BikeOrder.java
@@ -7,19 +7,24 @@ public class BikeOrder {
 		return order;
 	}
 	
+	public String Okay(String bikeModel, int quantity){
+		order = "Your order is valid: Model - " + bikeModel + ", Quantity - " + quantity;
+		return order;
+	}
+	
 	public void validateOrder(String bikeModel, int quantity) throws TooManyBikesException {
 		
 		if ((bikeModel == "Ukraina") && (quantity <=4)) {
-			order = "Your order is valid: Model - " + bikeModel + ", Quantity - " + quantity;
+			Okay(bikeModel, quantity);
 		}
 		else if ((bikeModel == "Aist") && (quantity <=5)) {
-			order = "Your order is valid: Model - " + bikeModel + ", Quantity - " + quantity;
+			Okay(bikeModel, quantity);
 		}
 		else if ((bikeModel == "Ardis") && (quantity <=7)) {
-			order = "Your order is valid: Model - " + bikeModel + ", Quantity - " + quantity;
+			Okay(bikeModel, quantity);
 		}
 		else if ((bikeModel == "Bergamont") && (quantity <=6)) {
-			order = "Your order is valid: Model - " + bikeModel + ", Quantity - " + quantity;
+			Okay(bikeModel, quantity);
 		}
 		else {
 			throw new TooManyBikesException(" Can't ship package. Max Ukraina:4, Aist:5, Ardis:7, Bergamont:6.");
